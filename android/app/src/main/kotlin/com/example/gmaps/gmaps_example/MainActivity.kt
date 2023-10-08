3acce28ea60a6cdc8abd226182413e1c1fa2694f
Andy Ponce <andy.ponce@outlook.com>
2023-10-08 09:44:35 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gmaps/gmaps_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gmaps.gmaps_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:gmaps_example/CodeLabsGmapsFlutterPage.dart';
+import 'package:gmaps_example/G4G_GmapsPage.dart';
+import 'package:gmaps_example/Maxonflutter_main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // on below line we are specifying title of our app
+      title: 'GFG',
+      // on below line we are hiding debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // on below line we are specifying theme
+        primarySwatch: Colors.green,
+      ),
+      // First screen of our app
+      home: Maxonflutter_main(),
+    );
+  }
+}
